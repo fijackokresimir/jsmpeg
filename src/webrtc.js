94c636388c14880eec327322b1ed5af927ed889f
Nikola Markezić <nikola.markezic@live.com>
2020-07-01 14:05:10 +0200

    improvments

--- src/webrtc.js
+++ src/webrtc.js
@@ -1,99 +1,140 @@
-JSMpeg.Source.WebRTC = (function() { "use strict";
+JSMpeg.Source.WebRTC = (function () {
+    "use strict";
 
-var WebRTCSource = function (options) {
-    this.options = options;
-    this.rtcPeerConnection = options.rtcPeerConnection;
-    this.rtcDataChannel = options.rtcDataChannel;
-    this.streaming = true;
+    var WebRTCSource = function (options) {
+        this.options = options;
+        this.rtcPeerConnection = options.rtcPeerConnection;
+        this.rtcDataChannel = options.rtcDataChannel;
+        this.streaming = true;
 
-    this.callbacks = {connect: [], data: []};
-    this.destination = null;
+        this.callbacks = { connect: [], data: [] };
+        this.destination = null;
 
-    this.reconnectInterval = options.reconnectInterval !== undefined
-        ? options.reconnectInterval
-        : 5;
-    this.shouldAttemptReconnect = !!this.reconnectInterval;
+        this.reconnectInterval = options.reconnectInterval !== undefined
+            ? options.reconnectInterval
+            : 5;
 
-    this.completed = false;
-    this.established = true;
-    this.progress = 0;
+        this.connectionTimeoutInterval = options.connectionTimeoutInterval !== undefined
+            ? options.connectionTimeoutInterval
+            : 5;
 
-    this.reconnectTimeoutId = 0;
+        this.shouldAttemptReconnect = !!this.reconnectInterval;
 
-    this.onEstablishedCallback = options.onSourceEstablished;
-    this.onCompletedCallback = options.onSourceCompleted; // Never used
-};
+        this.completed = false;
+        this.established = true;
+        this.progress = 0;
 
-WebRTCSource.prototype.connect = function(destination) {
-    this.destination = destination;
-};
+        this.reconnectTimeoutId = 0;
+        this.connectionTimeoutId = 0;
 
-WebRTCSource.prototype.destroy = function () {
-    clearTimeout(this.reconnectTimeoutId);
-    this.shouldAttemptReconnect = false;
-    this.rtcDataChannel.close();
-    this.rtcPeerConnection.close();
-    this.rtcPeerConnection = null;
-};
+        this.onEstablishedCallback = options.onSourceEstablished;
+        this.onCompletedCallback = options.onSourceCompleted; // Never used
+        this.onErrorCallback = options.onErrorCallback;
+        this.onCloseCallback = options.onCloseCallback;
+    };
 
-WebRTCSource.prototype.start = function() {
-    this.shouldAttemptReconnect = !!this.reconnectInterval;
-    this.progress = 0;
-    this.established = false;
+    WebRTCSource.prototype.connect = function (destination) {
+        this.destination = destination;
+    };
 
-    // CHROME SAYS IT DOESN'T SUPPORT BLOB SO WE CAN'T HAVE THIS COMMAND BUT PROGRAM STILL WORKS
-    // this.rtcDataChannel.binaryType = 'blob';
-    this.rtcDataChannel.onopen = this.onOpen.bind(this);
-    this.rtcDataChannel.onmessage = this.onMessage.bind(this);
-    this.rtcDataChannel.onclose = this.onClose.bind(this);
-    this.rtcDataChannel.onerror = this.onClose.bind(this);
-};
+    WebRTCSource.prototype.destroy = function () {
+        clearTimeout(this.reconnectTimeoutId);
+		clearTimeout(this.connectionTimeoutId);
+        this.shouldAttemptReconnect = false;
+        if (this.rtcDataChannel) {
+            this.rtcDataChannel.close();
+        }
+        if (this.rtcPeerConnection) {
+            this.rtcPeerConnection.close();
+        }
+        this.rtcPeerConnection = null;
+    };
 
-WebRTCSource.prototype.write = function(buffer) {
-};
+    WebRTCSource.prototype.start = function () {
+        this.progress = 0;
+        this.established = false;
 
-WebRTCSource.prototype.resume = function(headroom) {
-    // Nothing to do here
-};
+        // CHROME SAYS IT DOESN'T SUPPORT BLOB SO WE CAN'T HAVE THIS COMMAND BUT PROGRAM STILL WORKS
+        // this.rtcDataChannel.binaryType = 'blob';
+        this.rtcDataChannel.onopen = this.onOpen.bind(this);
+        this.rtcDataChannel.onmessage = this.onMessage.bind(this);
+        this.rtcDataChannel.onclose = this.onClose.bind(this);
+        this.rtcDataChannel.onerror = this.onError.bind(this);
+    };
 
-WebRTCSource.prototype.onOpen = function() {
-    this.progress = 1;
-};
+    WebRTCSource.prototype.write = function (buffer) {
+    };
 
-WebRTCSource.prototype.onClose = function() {
-  if (this.shouldAttemptReconnect) {
-      clearTimeout(this.reconnectTimeoutId);
-      this.reconnectTimeoutId = setTimeout(function(){
-          this.start();
-      }.bind(this), this.reconnectInterval*1000);
-  }
-};
+    WebRTCSource.prototype.resume = function (headroom) {
+        // Nothing to do here
+    };
 
-WebRTCSource.prototype.established = function() {
-    // Nothing to do here
-};
+    WebRTCSource.prototype.onOpen = function () {
+        this.progress = 1;
+        this.connectionTimeout();
+    };
 
-WebRTCSource.prototype.completed = function() {
-    // Nothing to do here
-};
+    WebRTCSource.prototype.onClose = function () {
+        if (this.shouldAttemptReconnect) {
+            this.shouldAttemptReconnect = false;
+            clearTimeout(this.reconnectTimeoutId);
+            this.reconnectTimeoutId = setTimeout(function () {
+                this.start();
+            }.bind(this), this.reconnectInterval * 1000);
+        } else if (this.onCloseCallback) {
+            this.onCloseCallback(this);
+        }
+    };
 
-WebRTCSource.prototype.progress = function() {
-    // Nothing to do here
-};
+    WebRTCSource.prototype.onError = function () {
+        if (this.shouldAttemptReconnect) {
+            this.shouldAttemptReconnect = false;
+            clearTimeout(this.reconnectTimeoutId);
+            this.reconnectTimeoutId = setTimeout(function () {
+                this.start();
+            }.bind(this), this.reconnectInterval * 1000);
+        } else if (this.onErrorCallback) {
+            this.onErrorCallback(this);
+        }
+    };
 
-WebRTCSource.prototype.onMessage = async function (evt) {
-    var isFirstChunk = !this.established;
-    this.established = true;
+    WebRTCSource.prototype.established = function () {
+        // Nothing to do here
+    };
 
-    if (isFirstChunk && this.onEstablishedCallback) {
-        this.onEstablishedCallback(this);
+    WebRTCSource.prototype.completed = function () {
+        // Nothing to do here
+    };
+
+    WebRTCSource.prototype.progress = function () {
+        // Nothing to do here
+    };
+
+    WebRTCSource.prototype.onMessage = async function (evt) {
+        this.shouldAttemptReconnect =true;
+        var isFirstChunk = !this.established;
+        this.established = true;
+
+        this.connectionTimeout();
+
+        if (isFirstChunk && this.onEstablishedCallback) {
+            this.onEstablishedCallback(this);
+        }
+
+        if (this.destination) {
+            this.destination.write(await new Response(evt.data).arrayBuffer());
+        }
+    };
+
+    WebRTCSource.prototype.connectionTimeout = function () {
+        clearTimeout(this.connectionTimeoutId);
+        this.connectionTimeoutId = setTimeout(() => {
+            if (this.onErrorCallback) {
+                this.onErrorCallback();
+            }
+        }, this.connectionTimeoutInterval * 1000);
     }
 
-    if (this.destination) {
-        this.destination.write(await new Response(evt.data).arrayBuffer());
-    }
-};
-
-return WebRTCSource;
+    return WebRTCSource;
 
 })();
--- src/websocket.js
+++ src/websocket.js
@@ -1,83 +1,121 @@
-JSMpeg.Source.WebSocket = (function(){ "use strict";
+const { LoaderOptionsPlugin } = require("webpack");
 
-var WSSource = function(url, options) {
-	this.url = url;
-	this.options = options;
-	this.socket = null;
-	this.streaming = true;
+JSMpeg.Source.WebSocket = (function () {
+	"use strict";
 
-	this.callbacks = {connect: [], data: []};
-	this.destination = null;
+	var WSSource = function (url, options) {
+		this.url = url;
+		this.options = options;
+		this.socket = null;
+		this.streaming = true;
 
-	this.reconnectInterval = options.reconnectInterval !== undefined
-		? options.reconnectInterval
-		: 5;
-	this.shouldAttemptReconnect = !!this.reconnectInterval;
+		this.callbacks = { connect: [], data: [] };
+		this.destination = null;
 
-	this.completed = false;
-	this.established = false;
-	this.progress = 0;
+		this.reconnectInterval = options.reconnectInterval !== undefined
+			? options.reconnectInterval
+			: 5;
+		this.shouldAttemptReconnect = !!this.reconnectInterval;
 
-	this.reconnectTimeoutId = 0;
+		this.completed = false;
+		this.established = false;
+		this.progress = 0;
 
-	this.onEstablishedCallback = options.onSourceEstablished;
-	this.onCompletedCallback = options.onSourceCompleted; // Never used
-};
+		this.reconnectTimeoutId = 0;
+		this.connectionTimeoutId = 0;
 
-WSSource.prototype.connect = function(destination) {
-	this.destination = destination;
-};
+		this.connectionTimeoutInterval = options.connectionTimeoutInterval !== undefined
+			? options.connectionTimeoutInterval
+			: 5;
 
-WSSource.prototype.destroy = function() {
-	clearTimeout(this.reconnectTimeoutId);
-	this.shouldAttemptReconnect = false;
-	this.socket.close();
-};
+		this.onEstablishedCallback = options.onSourceEstablished;
+		this.onCompletedCallback = options.onSourceCompleted; // Never used
+		this.onClosedCallback = options.onClosedCallback;
+		this.onErrorCallback = options.onErrorCallback;
+	};
 
-WSSource.prototype.start = function() {
-	this.shouldAttemptReconnect = !!this.reconnectInterval;
-	this.progress = 0;
-	this.established = false;
-	
-	this.socket = new WebSocket(this.url, this.options.protocols || null);
-	this.socket.binaryType = 'arraybuffer';
-	this.socket.onmessage = this.onMessage.bind(this);
-	this.socket.onopen = this.onOpen.bind(this);
-	this.socket.onerror = this.onClose.bind(this);
-	this.socket.onclose = this.onClose.bind(this);
-};
+	WSSource.prototype.connect = function (destination) {
+		this.destination = destination;
+	};
 
-WSSource.prototype.resume = function(secondsHeadroom) {
-	// Nothing to do here
-};
-
-WSSource.prototype.onOpen = function() {
-	this.progress = 1;
-};
-
-WSSource.prototype.onClose = function() {
-	if (this.shouldAttemptReconnect) {
+	WSSource.prototype.destroy = function () {
 		clearTimeout(this.reconnectTimeoutId);
-		this.reconnectTimeoutId = setTimeout(function(){
-			this.start();	
-		}.bind(this), this.reconnectInterval*1000);
+		clearTimeout(this.connectionTimeoutId);
+		this.shouldAttemptReconnect = false;
+		this.socket.close();
+	};
+
+	WSSource.prototype.start = function () {
+		this.progress = 0;
+		this.established = false;
+
+		this.socket = new WebSocket(this.url, this.options.protocols || null);
+		this.socket.binaryType = 'arraybuffer';
+		this.socket.onmessage = this.onMessage.bind(this);
+		this.socket.onopen = this.onOpen.bind(this);
+		this.socket.onerror = this.onError.bind(this);
+		this.socket.onclose = this.onClose.bind(this);
+	};
+
+	WSSource.prototype.resume = function (secondsHeadroom) {
+		// Nothing to do here
+	};
+
+	WSSource.prototype.onOpen = function () {
+		this.progress = 1;
+		this.connectionTimeout();
+	};
+
+	WSSource.prototype.onClose = function () {
+		if (this.shouldAttemptReconnect) {
+			// this.shouldAttemptReconnect = false;
+			clearTimeout(this.reconnectTimeoutId);
+			this.reconnectTimeoutId = setTimeout(function () {
+				this.start();
+			}.bind(this), this.reconnectInterval * 1000);
+		} else if (this.onClosedCallback) {
+			this.onClosedCallback(this);
+		}
+	};
+
+	WSSource.prototype.onError = function () {
+		if (this.shouldAttemptReconnect) {
+			// this.shouldAttemptReconnect = false;
+			clearTimeout(this.reconnectTimeoutId);
+			this.reconnectTimeoutId = setTimeout(function () {
+				this.start();
+			}.bind(this), this.reconnectInterval * 1000);
+		} else if (this.onErrorCallback) {
+			this.onErrorCallback(this);
+		}
 	}
-};
 
-WSSource.prototype.onMessage = function(ev) {
-	var isFirstChunk = !this.established;
-	this.established = true;
+	WSSource.prototype.onMessage = function (ev) {
+		var isFirstChunk = !this.established;
+		this.established = true;
+		this.shouldAttemptReconnect = true;
+	
+		this.connectionTimeout();
 
-	if (isFirstChunk && this.onEstablishedCallback) {
-		this.onEstablishedCallback(this);
+		if (isFirstChunk && this.onEstablishedCallback) {
+			this.onEstablishedCallback(this);
+		}
+
+		if (this.destination) {
+			this.destination.write(ev.data);
+		}
+	};
+
+	WSSource.prototype.connectionTimeout = function () {
+		clearTimeout(this.connectionTimeoutId);
+		this.connectionTimeoutId = setTimeout(() => {
+			if (this.onErrorCallback) {
+			this.onErrorCallback();		
+			}	
+		}, this.connectionTimeoutInterval * 1000);
 	}
 
-	if (this.destination) {
-		this.destination.write(ev.data);
-	}
-};
-
-return WSSource;
+	return WSSource;
 
 })();
 
